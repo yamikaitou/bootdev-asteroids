fbefc03118d2e8d791106a99ac6cbcbf17b09599
YamiKaitou <git@yamikaitou.dev>
2024-11-02 09:56:52 -0400

    use screen object instead

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pygame
 from constants import *
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        pygame.Surface.fill(screen, (0,0,0))
+
+        screen.fill("black")
         pygame.display.flip()
 
 
